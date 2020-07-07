a7b7f547583ade608abac25c3bc7b660b400c8ad
Florian Wyss <fw@studer-raimann.ch>
2020-07-07 15:41:37 +0200

    WIP

--- src/GitCurl.php
+++ src/GitCurl.php
@@ -66,44 +66,66 @@ final class GitCurl
      *
      * @return string|null
      */
-    public function fetchFile(string $path)/*: ?string*/
+    public function fetchFile(string $path) : ?string
     {
+        $url = $this->url . "/" . $this->default_branch . "/" . $path;
+
+        $headers = [];
+
+        $result = $this->doRequest($url, $headers);
+
+        if (empty($result)) {
+            return null;
+        }
+
+        return $result;
+    }
+
+
+    /**
+     *
+     */
+    public function getDefaultBranch() : void
+    {
+        // Supports only github api
+        if (strpos($this->url, "github.com") !== false) {
+            return;
+        }
+
+        $url = str_replace("github.com", "api.github.com/repos", $this->url);
+
+        $headers = [
+            "Accept" => "application/json"
+        ];
+
+        $result = $this->doRequest($url, $headers);
+
+        $result_json = json_decode($result, true);
+
+        if (is_array($result_json) && is_string($result_json["default_branch"]) && !empty($result_json["default_branch"])) {
+            $this->default_branch = $result_json["default_branch"];
+        }
+    }
+
+
+    /**
+     * @param string $url
+     * @param array  $headers
+     *
+     * @return string|null
+     */
+    private function doRequest(string $url, array $headers) : ?string
+    {
+        $curlConnection = null;
+
         try {
-            $url = $this->url . "/" . $this->default_branch . "/" . $path;
-
-            $curlConnection = new ilCurlConnection();
-
-            $curlConnection->init();
-
-            // use a proxy, if configured by ILIAS
-            if (!self::version()->is6()) {
-                $proxy = ilProxySettings::_getInstance();
-                if ($proxy->isActive()) {
-                    $curlConnection->setOpt(CURLOPT_HTTPPROXYTUNNEL, true);
-
-                    if (!empty($proxy->getHost())) {
-                        $curlConnection->setOpt(CURLOPT_PROXY, $proxy->getHost());
-                    }
-
-                    if (!empty($proxy->getPort())) {
-                        $curlConnection->setOpt(CURLOPT_PROXYPORT, $proxy->getPort());
-                    }
-                }
-            }
-
-            $curlConnection->setOpt(CURLOPT_RETURNTRANSFER, true);
-            $curlConnection->setOpt(CURLOPT_URL, $url);
+            $curlConnection = $this->initCurlConnection($url, $headers);
 
             $result = $curlConnection->exec();
 
-            if (is_string($result) && !empty($result)) {
-                return $result;
-            } else {
-                return null;
-            }
+            return $result;
         } catch (Throwable $ex) {
         } finally {
-            // Close Curl connection
             if ($curlConnection !== null) {
                 $curlConnection->close();
                 $curlConnection = null;
@@ -117,67 +139,7 @@ final class GitCurl
     /**
      *
      */
-    public function getDefaultBranch()/*: void*/
-    {
-        try {
-            // Supports only github api
-            if (strpos($this->url, "github.com") !== false) {
-                $url = str_replace("github.com", "api.github.com/repos", $this->url);
-
-                $curlConnection = new ilCurlConnection();
-
-                $curlConnection->init();
-
-                // use a proxy, if configured by ILIAS
-                if (!self::version()->is6()) {
-                    $proxy = ilProxySettings::_getInstance();
-                    if ($proxy->isActive()) {
-                        $curlConnection->setOpt(CURLOPT_HTTPPROXYTUNNEL, true);
-
-                        if (!empty($proxy->getHost())) {
-                            $curlConnection->setOpt(CURLOPT_PROXY, $proxy->getHost());
-                        }
-
-                        if (!empty($proxy->getPort())) {
-                            $curlConnection->setOpt(CURLOPT_PROXYPORT, $proxy->getPort());
-                        }
-                    }
-                }
-
-                $curlConnection->setOpt(CURLOPT_RETURNTRANSFER, true);
-                $curlConnection->setOpt(CURLOPT_URL, $url);
-
-                $headers = [
-                    "Accept"     => "application/json",
-                    "User-Agent" => "ILIAS " . self::version()->getILIASVersion()
-                ];
-                $headers = array_map(function ($key, $value) {
-                    return ($key . ": " . $value);
-                }, array_keys($headers), $headers);
-                $curlConnection->setOpt(CURLOPT_HTTPHEADER, $headers);
-
-                $result = $curlConnection->exec();
-
-                $result = json_decode($result, true);
-                if (is_array($result) && is_string($result["default_branch"]) && !empty($result["default_branch"])) {
-                    $this->default_branch = $result["default_branch"];
-                }
-            }
-        } catch (Throwable $ex) {
-        } finally {
-            // Close Curl connection
-            if ($curlConnection !== null) {
-                $curlConnection->close();
-                $curlConnection = null;
-            }
-        }
-    }
-
-
-    /**
-     *
-     */
-    private function fixUrl()/*: void*/
+    private function fixUrl() : void
     {
         // Fix possible windows paths
         $this->url = str_replace("\\", "/", $this->url);
@@ -197,5 +159,47 @@ final class GitCurl
 
         // Get github raw file content
         $this->url = str_replace("github.com", "raw.githubusercontent.com", $this->url);
+    }
+
+
+    /**
+     * @param string $url
+     * @param array  $headers
+     *
+     * @return ilCurlConnection
+     */
+    private function initCurlConnection(string $url, array $headers) : ilCurlConnection
+    {
+        $curlConnection = new ilCurlConnection($url);
+
+        $curlConnection->init();
+
+        if (!self::version()->is6()) {
+            $proxy = ilProxySettings::_getInstance();
+            if ($proxy->isActive()) {
+                $curlConnection->setOpt(CURLOPT_HTTPPROXYTUNNEL, true);
+
+                if (!empty($proxy->getHost())) {
+                    $curlConnection->setOpt(CURLOPT_PROXY, $proxy->getHost());
+                }
+
+                if (!empty($proxy->getPort())) {
+                    $curlConnection->setOpt(CURLOPT_PROXYPORT, $proxy->getPort());
+                }
+            }
+        }
+
+        $headers["User-Agent"] = "ILIAS " . self::version()->getILIASVersion();
+        $curlConnection->setOpt(CURLOPT_HTTPHEADER, array_map(function (string $key, string $value) : string {
+            return ($key . ": " . $value);
+        }, array_keys($headers), $headers));
+
+        $curlConnection->setOpt(CURLOPT_FOLLOWLOCATION, true);
+
+        $curlConnection->setOpt(CURLOPT_RETURNTRANSFER, true);
+
+        $curlConnection->setOpt(CURLOPT_VERBOSE, false/*(intval(DEVMODE) === 1)*/);
+
+        return $curlConnection;
     }
 }
